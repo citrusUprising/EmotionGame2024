f50443cac281abf48ff765af9dc4fcfd18ebd134
citrusUprising <62976444+citrusUprising@users.noreply.github.com>
2024-04-11 13:37:08 -0700

    added enums

--- TetriminoesEmotionalGame/Assets/Scripts/ManageState.cs
+++ TetriminoesEmotionalGame/Assets/Scripts/ManageState.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class ManageState : MonoBehaviour
 {
+    public enum State
+    {
+        start = 0,
+        end = 1,
+        personEnter = 2,
+        personLeave = 3,
+        sitting = 4,
+        talking = 5
+    };
+    public enum Symbols
+    {
+        square = 0,
+        circle = 1,
+        triangle = 2,
+        x = 3,
+        squareX = 4,
+        squareTri = 5,
+        squareCirc = 6,
+        circleX = 7,
+        circleTri = 8,
+        triangleX = 9
+
+    };
+    public GameObject camera;
+    private State game;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        game = State.start;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch(game){
+            case State.start: //Opening cinematic
+            break;
+
+            case State.personEnter: //when a new person is entering the train
+            break;
+
+            case State.sitting: //when the train is moving but a conversation hasn't started
+            break;
+
+            case State.talking: //when the player is in conversation
+            break;
+
+            case State.personLeave: //when a person is exiting the train
+            break;
+
+            case State.end: //closing cinematic
+            break;
+
+            default:
+            break;
+        }
     }
 }
---
+++ TetriminoesEmotionalGame/Assets/Scripts/PersonHandler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PersonHandler : MonoBehaviour
+{
+    public int affection = 0; // caps at -1 and 100
+    public Image[] spriteSheet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
